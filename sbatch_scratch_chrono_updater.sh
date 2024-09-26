90b27b4c6282aee221cc8ef180636a64fdec0717
lorenzbaraldi <lorenz.baraldi@gmail.com>
2024-09-26 10:48:11 +0200

    Added multiple directories

--- sbatch_scratch_chrono_updater.sh
+++ sbatch_scratch_chrono_updater.sh
@@ -12,16 +12,16 @@
 #SBATCH --array=0-2%3
 
 paths=(
-# "/leonardo_scratch/large/userexternal/fcocchi0/dpo"
-# "/leonardo_scratch/large/userexternal/fcocchi0/safe-clip"
+    "/leonardo_scratch/large/userexternal/lbarald1/dire"
+    "/leonardo_scratch/large/userexternal/lbarald1/Dit-3"
 # "/leonardo_scratch/large/userexternal/fcocchi0/sdan"
-    "/leonardo_scratch/large/userexternal/lbarald1/mapet/log_run"
+    "/leonardo_scratch/large/userexternal/lbarald1/mapet"
 )
 
 path=${paths[$SLURM_ARRAY_TASK_ID]}
 
-echo "Run metadata changes for user lbarald1 on partition lrd_all_serial for path $paths"
+echo "Run metadata changes for user lbarald1 on partition lrd_all_serial for path $path"
 
 # cd /leonardo/home/userexternal/tpoppi00/scripts
 
-./update_metadata.sh "$paths"
+./update_metadata.sh "$path"
